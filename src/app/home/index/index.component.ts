20dbc78c9ac34332594af0c4f80f11ea8bf3d91b
Dhruvish Patel <dhruvish200115@gmail.com>
2023-03-24 12:04:16 +0530

    final update

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HomeService } from 'src/app/core/services/home.services';
 
 @Component({
   selector: 'app-index',
@@ -9,16 +10,44 @@ import { Router } from '@angular/router';
 export class IndexComponent implements OnInit {
 
 
+  staticURL: any = '';
+  choice: any;
   constructor(
-    private router:Router
-  ) { }
+    private router: Router,
+    private homeService: HomeService
+
+  ) {
+  }
 
   ngOnInit(): void {
   }
-  showDiv(id:any){
-    localStorage.setItem('choice',id);
-    this.router.navigate(['/home/main']);
+  showDiv(id: any) {
+    localStorage.clear();
+    localStorage.setItem('choice', id);
+    this.getInstituteDetails();
+  }
 
+  getInstituteDetails() {
+    this.choice = localStorage.getItem('choice');
+    if (this.choice == 'primary') {
+      this.staticURL = 'www.mgvidhyalayaprimary.ac.in';
+      this.homeService.getInstituteDetailsById(this.staticURL).subscribe((res: any) => {
+        localStorage.setItem('InstituteId', res[0].id);
+        localStorage.setItem('InstituteName', res[0].name);
+        localStorage.setItem('InstituteURL', res[0].url);
+        this.router.navigate(['/home/main']);
+
+      })
+    }
+    else if (this.choice == 'secondary') {
+      this.staticURL = 'www.mgvidhyalayahs.ac.in';
+      this.homeService.getInstituteDetailsById(this.staticURL).subscribe((res: any) => {
+        localStorage.setItem('InstituteId', res[0].id);
+        localStorage.setItem('InstituteName', res[0].name);
+        localStorage.setItem('InstituteURL', res[0].url);
+        this.router.navigate(['/home/main']);
+      })
+    }
 
   }
 }
--- src/app/more/more.module.ts
+++ src/app/more/more.module.ts
@@ -8,6 +8,14 @@ import { MoreRoutes } from './more.routing';
 import { SearchComponent } from './search/search.component';
 import { EvalutionComponent } from './evalution/evalution.component';
 import { LibraryComponent } from './library/library.component';
+import { AcademicCalendarComponent } from './academic-calendar/academic-calendar.component';
+import { MagazineComponent } from './magazine/magazine.component';
+import { NewsComponent } from './news/news.component';
+import { PaperComponent } from './paper/paper.component';
+import { ResultComponent } from './result/result.component';
+import { ScholarshipsComponent } from './scholarships/scholarships.component';
+import { SyllabusComponent } from './syllabus/syllabus.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 
@@ -16,13 +24,21 @@ import { LibraryComponent } from './library/library.component';
     CampusComponent,
     SearchComponent,
     EvalutionComponent,
-    LibraryComponent
+    LibraryComponent,
+    AcademicCalendarComponent,
+    MagazineComponent,
+    NewsComponent,
+    PaperComponent,
+    ResultComponent,
+    ScholarshipsComponent,
+    SyllabusComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule.forChild(MoreRoutes),
     NgbModule,
+    NgxPaginationModule
   ]
 })
 export class MoreModule { }
